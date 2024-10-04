295348de5d2e80d10374a2f02a681715bb9382b4
EliteMasterEric <ericmyllyoja@gmail.com>
2024-10-04 07:58:21 -0400

    Fix duplicate combo counter on combo break
    Co-authored-by: Eric <ericmyllyoja@gmail.com>
    Co-authored-by: Hundrec <hundrecard@gmail.com>

--- source/funkin/play/PlayState.hx
+++ source/funkin/play/PlayState.hx
@@ -1457,6 +1457,8 @@ class PlayState extends MusicBeatSubState
     if (FlxG.sound.music != null)
     {
       var correctSync:Float = Math.min(FlxG.sound.music.length, Math.max(0, Conductor.instance.songPosition - Conductor.instance.combinedOffset));
+      var playerVoicesError:Float = 0;
+      var opponentVoicesError:Float = 0;
 
       var playerVoicesError:Float = 0;
       var opponentVoicesError:Float = 0;
@@ -2563,8 +2565,8 @@ class PlayState extends MusicBeatSubState
         healthChange = Constants.HEALTH_BAD_BONUS;
         isComboBreak = Constants.JUDGEMENT_BAD_COMBO_BREAK;
       case 'shit':
-        isComboBreak = Constants.JUDGEMENT_SHIT_COMBO_BREAK;
         healthChange = Constants.HEALTH_SHIT_BONUS;
+        isComboBreak = Constants.JUDGEMENT_SHIT_COMBO_BREAK;
     }
 
     // Send the note hit event.
@@ -2633,8 +2635,6 @@ class PlayState extends MusicBeatSubState
       }
     }
     vocals.playerVolume = 0;
-
-    if (Highscore.tallies.combo != 0) if (Highscore.tallies.combo >= 10) comboPopUps.displayCombo(0);
 
     applyScore(-10, 'miss', healthChange, true);
 
@@ -2854,7 +2854,7 @@ class PlayState extends MusicBeatSubState
       }
     }
     comboPopUps.displayRating(daRating);
-    if (combo >= 10 || combo == 0) comboPopUps.displayCombo(combo);
+    if (combo >= 10) comboPopUps.displayCombo(combo);
 
     vocals.playerVolume = 1;
   }
